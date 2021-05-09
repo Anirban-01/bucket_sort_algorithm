ad92edc4fd26af050106a6092bff20861ffbd6b2
Python learner <areyaban@gmail.com>
2021-05-09 22:09:28 +0530

    Add files via upload

---
+++ Bucket_sort_algorithm.py
@@ -0,0 +1,29 @@
+# bucket sort algorithm
+def bucket_function(array):
+    bucket = []
+
+    # Create empty buckets
+    for i in range(len(array)):
+        bucket.append([])
+
+    # Insert elements into their respective buckets
+    for j in array:
+        index = int(10 * j)
+        bucket[index].append(j)
+
+    # Sort the elements of each bucket
+    for i in range(len(array)):
+        bucket[i] = sorted(bucket[i])
+
+    # Get the sorted elements
+    k = 0
+    for i in range(len(array)):
+        for j in range(len(bucket[i])):
+            array[k] = bucket[i][j]
+            k += 1
+    return array
+
+
+array = [.42, .32, .33, .52, .37, .47, .51]
+print(len(array))
+print(bucket_function(array))
